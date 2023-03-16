4ebd0d7b6c1218c0038d83423e34c7862f29f49c
malissab <malissambishop7@gmail.com>
2023-03-15 19:20:15 -0700

    view more fixed

--- src/App.tsx
+++ src/App.tsx
@@ -24,9 +24,9 @@ export function App() {
     transactionsByEmployeeUtils.invalidateData()
 
     await employeeUtils.fetchAll()
-    setIsLoading(false)
-    
     await paginatedTransactionsUtils.fetchAll()
+
+    setIsLoading(false)
   }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
 
   const loadTransactionsByEmployee = useCallback(
@@ -77,7 +77,7 @@ export function App() {
           {transactions !== null && (
             <button
               className="RampButton"
-              disabled={paginatedTransactionsUtils.loading}
+              disabled={paginatedTransactionsUtils.loading || paginatedTransactions?.nextPage == null || transactionsByEmployee?.length === 0}
               onClick={async () => {
                 await loadAllTransactions()
               }}
